5a930cd6a4f38653034a82b33540d2b3cb6756e1
Sergey4442 <seregahramov2004@gmail.com>
2024-12-17 14:39:20 +0600

    new file:   actions/create-board.ts 	new file:   app/(platform)/(dashboard)/organization/[organizationId]/board.tsx 	modified:   app/(platform)/(dashboard)/organization/[organizationId]/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,14 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
-const OrganizationIdPage = () => {
-    async function create(formData: FormData) {
-        "use server";
-        const title = formData.get("title") as string;
+import { Board } from "./board";
 
-        await db.board.create({
-            data: {
-                title,
-            }
-        });
-    };
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
 
     return (
-        <div>
-            <form>
+        <div className="flex flex-col space-y-4">
+            <form action={create}>
                 <input 
                 id="title"
                 name="title"
@@ -21,8 +16,15 @@ const OrganizationIdPage = () => {
                 placeholder="Enter a board title"
                 className="border-black border p-1"
                 />
-
+                <Button type="submit">
+                    Submit
+                </Button>
             </form>
+            <div className="space-y-2">
+                {boards.map((board) => (
+                    <Board key={board.id} title={board.title} id={board.id} />
+                ))}
+            </div>
         </div>
     );
 };
